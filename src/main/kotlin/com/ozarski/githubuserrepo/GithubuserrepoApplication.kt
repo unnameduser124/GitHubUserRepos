4ee38e78f9b5624d86fe2fbf501989de99b9464d
ozarskiw <ozarskiw@gmail.com>
2024-02-15 11:11:16 +0100

    Initial commit

---
+++ src/main/kotlin/com/ozarski/githubuserrepo/GithubuserrepoApplication.kt
@@ -0,0 +1,11 @@
+package com.ozarski.githubuserrepo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubuserrepoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubuserrepoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ozarski/githubuserrepo/GithubuserrepoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ozarski.githubuserrepo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubuserrepoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
